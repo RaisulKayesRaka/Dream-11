6371c8efc6404f6d3225e21b50d5b8d113595ae3
Raisul Kayes Raka <119594099+RaisulKayesRaka@users.noreply.github.com>
2024-10-26 10:22:41 +0600

    Make 'Newsletter' section responsive

--- src/Newsletter.jsx
+++ src/Newsletter.jsx
@@ -5,13 +5,13 @@ export default function Newsletter() {
             <div className="bg-white row-span-1"></div>
             <div className="bg-black row-span-1"></div>
         </section>
-        <section className="max-w-[1320px] mx-auto w-11/12 border border-white rounded-3xl p-6">
-        <div className="bg-gradient-to-tr from-cyan-200 via-white to-orange-200 rounded-2xl text-center p-20">
-            <h2 className="text-3xl font-bold mb-4">Subscribe to our Newsletter</h2>
-            <p className="text-lg text-[#131313B2] mb-6">Get the latest updates and news right in your inbox!</p>
-            <form action="" className="flex items-center justify-center gap-4">
-                <input className="max-w-sm w-full border rounded-xl px-3 py-2 md:px-4 md:py-3 lg:px-5 lg:py-4" type="email" placeholder="Enter your email" />
-                <button className="bg-gradient-to-br from-red-300 via-amber-300 to-yellow-200 shadow-sm text-black rounded-xl px-3 py-2 md:px-4 md:py-3 lg:px-5 lg:py-4 font-bold transition-transform active:scale-95" type="submit">Subscribe</button>
+        <section className="max-w-[1320px] mx-auto w-11/12 border border-white rounded-3xl p-4 md:p-5 lg:p-6">
+        <div className="bg-gradient-to-tr from-cyan-200 via-white to-orange-200 rounded-2xl text-center p-4 sm:p-8 md:p-16 lg:p-20">
+            <h2 className="text-xl md:text-2xl lg:text-3xl font-bold mb-4">Subscribe to our Newsletter</h2>
+            <p className="text-sm sm:text-base md:text-lg text-[#131313B2] mb-6">Get the latest updates and news right in your inbox!</p>
+            <form action="" className="flex items-center justify-center flex-wrap gap-4">
+                <input className="max-w-sm w-full border rounded-xl px-3 py-2 md:px-4 md:py-3 lg:px-5 lg:py-4 text-sm sm:text-base" type="email" placeholder="Enter your email" />
+                <button className="bg-gradient-to-br from-red-300 via-amber-300 to-yellow-200 shadow-sm text-black rounded-xl px-3 py-2 md:px-4 md:py-3 lg:px-5 lg:py-4 font-bold transition-transform active:scale-95 text-sm sm:text-base" type="submit">Subscribe</button>
             </form>
         </div>
         </section>
