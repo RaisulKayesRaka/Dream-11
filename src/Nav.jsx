f7942e2f99e394ccd525ebdf8470990360838538
Raisul Kayes Raka <119594099+RaisulKayesRaka@users.noreply.github.com>
2024-10-26 20:36:14 +0600

    Update navbar section

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -3,12 +3,17 @@ import PropTypes from "prop-types";
 export default function Nav({ claimCredit }) {
   return (
     <nav className="sticky top-0 z-10 bg-white/50 pb-4 pt-4 backdrop-blur-sm sm:pt-8 md:pb-5 md:pt-10 lg:pb-6 lg:pt-12">
-      <div className="mx-auto flex w-11/12 max-w-[1320px] items-center justify-between">
-        <img
-          className="w-12 md:w-16 lg:w-[72px]"
-          src="./assets/logo.svg"
-          alt=""
-        />
+      <div className="mx-auto flex w-11/12 max-w-[1320px] flex-col items-center justify-between gap-4 md:flex-row">
+        <div className="flex w-full items-center justify-between gap-12 md:w-auto">
+          <img
+            className="w-12 md:w-16 lg:w-[72px]"
+            src="./assets/logo.svg"
+            alt=""
+          />
+          <button className="md:hidden">
+            <img className="w-6" src="./assets/menu.svg" alt="" />
+          </button>
+        </div>
         <div className="flex items-center gap-12">
           <ul className="hidden items-center gap-12 md:flex">
             <li>
